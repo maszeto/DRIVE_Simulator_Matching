378d127c9c4933ed2b7b91ddc9783c1ccd494d32
Matt Szeto <mattaszeto@gmail.com>
2022-08-13 22:24:24 -0700

    Added more matching framework files

--- matchingToolbox/getViewedVehicles.m
+++ matchingToolbox/getViewedVehicles.m
@@ -1,6 +1,11 @@
 function [outputArg1,outputArg2] = getViewedVehicles(inputArg1,inputArg2)
 %GETVIEWEDVEHICLES Get vehicles in LiDAR LOS from input vehicle 
-%   Detailed explanation goes here
+%   For the current timestep, return a list of Vehicle ID's of each vehicle
+%   within the LiDAR LOS for every vehicle in the sim 
+%   Output Row index corresponds to vid and columns are the vehicles in
+%   that vehicle's LOS, the vehicles in view are candidates for the
+%   matching game
+
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 end
---
+++ matchingToolbox/stableFixtures.m
@@ -0,0 +1,10 @@
+function [outputArg1,outputArg2] = stableFixtures(inputArg1,inputArg2)
+%STABLEFIXTURES Summary of this function goes here
+%   Performs the stable fixtures matching game for a given timestep
+%   Inputs: utilityfunction, vehicles, candidates (vehicles in LOS),
+%   vehicleStructs
+%   Outputs: Vehicles and their matches 
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
