8627bb6f1128152a403f1548ac8acc52cc6de82a
Matt Szeto <mattaszeto@gmail.com>
2022-09-03 22:58:52 -0700

    Fixed corner case causing error with u_nearest

--- tools/getVehNodeDistance.m
+++ tools/getVehNodeDistance.m
@@ -3,13 +3,21 @@ function [distance] = getVehNodeDistance(vid1, vid2, vehiclesStruct,timeStep)
 %   Detailed explanation goes here
     %we need to find the index that correlates to the timestep for the
     %given time
-    idx1 = find(vehiclesStruct.vehNode(vid1).time==timeStep);
-    idx2 = find(vehiclesStruct.vehNode(vid2).time==timeStep);
-        
-    x1 = vehiclesStruct.vehNode(vid1).x(idx1);
-    y1 = vehiclesStruct.vehNode(vid1).y(idx1);
-    x2 = vehiclesStruct.vehNode(vid2).x(idx2);
-    y2 = vehiclesStruct.vehNode(vid2).y(idx2);
-    distance = abs(pdist([x1,y1;x2,y2], 'euclidean'));
+    
+    %Vehicle ID may be in view, but the id will not exist in vehiclesStruct
+    %because of how it is created in parseMobility
+    if (vid1 > length(vehiclesStruct.vehNode) || (vid2 > length(vehiclesStruct.vehNode)))
+        distance = [];
+    else
+    
+        idx1 = find(vehiclesStruct.vehNode(vid1).time==timeStep);
+        idx2 = find(vehiclesStruct.vehNode(vid2).time==timeStep);
+
+        x1 = vehiclesStruct.vehNode(vid1).x(idx1);
+        y1 = vehiclesStruct.vehNode(vid1).y(idx1);
+        x2 = vehiclesStruct.vehNode(vid2).x(idx2);
+        y2 = vehiclesStruct.vehNode(vid2).y(idx2);
+        distance = abs(pdist([x1,y1;x2,y2], 'euclidean'));
+    end
 end
 
